e05236d5d503419ea401bea888a16ffb7e69bae5
Yauheni Maksimau <yauheni.maksimau.work@gmail.com>
2023-05-13 14:30:13 +0300

    Added basic mapping

---
+++ Mapper.cs
@@ -0,0 +1,37 @@
+﻿namespace MapReduce;
+
+public class Mapper
+{
+    public void Map(string inputFilePath)
+    {
+        Dictionary<string, int> wordCounts = new();
+
+        using StreamReader reader = new(inputFilePath);
+        string? line;
+        while ((line = reader.ReadLine()) is not null)
+        {
+            string[] words = line.Split('\t');
+            foreach (string word in words)
+            {
+                if (!string.IsNullOrEmpty(word))
+                {
+                    if (wordCounts.ContainsKey(word))
+                    {
+                        wordCounts[word]++;
+                    }
+                    else
+                    {
+                        wordCounts[word] = 1;
+                    }
+                }
+            }
+        }
+
+        string outputFilePath = Path.ChangeExtension(inputFilePath, "_map.txt");
+        using StreamWriter writer = new(outputFilePath);
+        foreach (KeyValuePair<string, int> kvp in wordCounts)
+        {
+            writer.WriteLine($"{kvp.Key}\t{kvp.Value}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using MapReduce;
+
+Mapper mapper = new Mapper();
+string inputFilePath = Console.ReadLine()!;
+mapper.Map(inputFilePath);
+Console.WriteLine("Mapping completed.");
